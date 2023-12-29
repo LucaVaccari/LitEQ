da2317957e73ec2c244e356d04642f98ba9c09da
Luca <lucavaccari03@gmail.com>
2023-12-29 16:39:05 +0100

    Initial commit

---
+++ src/it/unibs/pajc/liteq/Main.java
@@ -0,0 +1,31 @@
+package it.unibs.pajc.liteq;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    private JFrame frame;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                Main window = new Main();
+                window.frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public Main() {
+        initialize();
+    }
+
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 450, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
