b45b78d7da4793e8220b78801ff8b1de6225c047
Luca <lucavaccari03@gmail.com>
2024-01-04 10:15:23 +0100

    Playback almost working

--- src/it/unibs/pajc/liteq/Main.java
+++ src/it/unibs/pajc/liteq/Main.java
@@ -2,6 +2,10 @@ package it.unibs.pajc.liteq;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class Main {
 
@@ -29,32 +33,88 @@ public class Main {
         frame = new JFrame();
         frame.setBounds(100, 100, 450, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         JPanel panel = new JPanel();
         frame.getContentPane().add(panel, BorderLayout.SOUTH);
         panel.setLayout(new GridLayout(0, 3, 0, 0));
-        
+
         JButton btnPlay = new JButton("Play");
         btnPlay.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(btnPlay);
-        
+
         JButton btnPause = new JButton("Pause");
         btnPause.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(btnPause);
-        
+
         JButton btnStop = new JButton("Stop");
         btnStop.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(btnStop);
-        
+
         JSeparator separator = new JSeparator();
         separator.setOrientation(SwingConstants.VERTICAL);
         panel.add(separator);
-        
+
         JSlider sliderSeek = new JSlider();
         sliderSeek.setValue(0);
         sliderSeek.setToolTipText("Seek playback");
         panel.add(sliderSeek);
 
         SoundPlayer player = new SoundPlayer();
+
+        btnPlay.addActionListener(l -> player.start());
+        btnPause.addActionListener(l -> player.pause());
+        btnStop.addActionListener(l -> player.stop());
+        sliderSeek.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                player.seek(sliderSeek.getValue());
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+        });
+        frame.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+                player.close();
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+            }
+        });
     }
 }
--- src/it/unibs/pajc/liteq/SoundPlayer.java
+++ src/it/unibs/pajc/liteq/SoundPlayer.java
@@ -1,18 +1,24 @@
 package it.unibs.pajc.liteq;
 
 import javax.sound.sampled.*;
+import java.io.Closeable;
 import java.io.File;
 
-public class SoundPlayer {
+public class SoundPlayer implements LineListener, Closeable {
     private static final String AUDIO_FILE_PATH = "C:\\Users\\asus\\Desktop\\SuckYourLollipop.wav";
 
     // TODO: make buffer size selectable
-    private static final int BUFFER_SIZE = 256;
+    private final int bufferSize = 256000;
+    private int playCursor = 0;
+    private byte[] bytes;
+    private boolean playing;
     private SourceDataLine dataLine;
+    private Runnable playbackProcess;
+    private Thread currentThread;
 
     public SoundPlayer() {
         try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(AUDIO_FILE_PATH))) {
-            byte[] bytes = audioInputStream.readAllBytes();
+            bytes = audioInputStream.readAllBytes();
             AudioFormat format = audioInputStream.getFormat();
 
             int duration =
@@ -21,10 +27,33 @@ public class SoundPlayer {
 
             DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
             dataLine = (SourceDataLine) AudioSystem.getLine(info);
-            dataLine.open(format);
-            dataLine.start();
+            dataLine.addLineListener(this);
+            dataLine.open(format, bufferSize);
 
-            //dataLine.write(bytes, 0, bytes.length);
+            //int dt = (int) (bufferSize * 000. / format.getSampleRate() / format.getSampleSizeInBits() / format
+            // .getChannels());
+            playbackProcess = () -> {
+                if (playCursor < 0) playCursor = 0;
+
+                while (playing) {
+                    if (playCursor < bytes.length) {
+                        int remainingBytes = bytes.length - playCursor;
+                        int currentBufferSize = Math.min(remainingBytes, bufferSize);
+                        currentBufferSize -= currentBufferSize % format.getFrameSize();
+
+                        byte[] buffer = new byte[currentBufferSize];
+                        System.arraycopy(bytes, playCursor, buffer, 0, currentBufferSize);
+                        dataLine.write(buffer, 0, currentBufferSize);
+                        if (playing) playCursor += bufferSize;
+                    }
+                }
+
+                playCursor -= dataLine.available();
+                dataLine.flush();
+            };
+
+            start();
+
         } catch (UnsupportedAudioFileException e) {
             System.out.println("Unsupported audio file");
         } catch (Exception e) {
@@ -32,15 +61,60 @@ public class SoundPlayer {
         }
     }
 
+    @Override
+    public void update(LineEvent event) {
+        LineEvent.Type type = event.getType();
+        if (type.equals(LineEvent.Type.CLOSE)) {
+            System.out.println("CLOSE");
+        } else if (type.equals(LineEvent.Type.OPEN)) {
+            System.out.println("OPEN");
+        } else if (type.equals(LineEvent.Type.START)) {
+            System.out.println("START");
+        } else if (type.equals(LineEvent.Type.STOP)) {
+            System.out.println("STOP");
+        }
+    }
+
     public void start() {
+        playing = false;
+        if (currentThread != null) {
+            try {
+                currentThread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        playing = true;
+        currentThread = new Thread(playbackProcess);
         dataLine.start();
+        currentThread.start();
+
+        System.out.println(playCursor);
+    }
+
+    public void pause() {
+        playing = false;
+        dataLine.stop();
+
+        System.out.println(playCursor);
     }
 
     public void stop() {
-        dataLine.stop();
+        pause();
+        seek(0);
     }
 
     public void seek(int percentage) {
+        pause();
+        playCursor = (int) (percentage / 100. * bytes.length);
+        start();
+    }
 
+    @Override
+    public void close() {
+        dataLine.flush();
+        stop();
+        dataLine.close();
     }
 }
